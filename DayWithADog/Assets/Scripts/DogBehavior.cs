80f7258e10e0634fc03f3dbbb5924114ebd2ac18
Lorien Cho <42721802+loriencho@users.noreply.github.com>
2022-12-03 17:38:18 -0500

    finished randomoized dog idling behavior, started toolbar + activities

--- DayWithADog/Assets/Scripts/DogBehavior.cs
+++ DayWithADog/Assets/Scripts/DogBehavior.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DogBehavior : MonoBehaviour
 {
@@ -16,18 +17,27 @@ public class DogBehavior : MonoBehaviour
     private bool idleMoving;
     private int movingDirection;
     private const int Right = 1;
-    private const int Left = 2;
+    private const int Left = -1;
+    [SerializeField]
+    private RectTransform rt;
+    [SerializeField]
+    private Image image;
+    [SerializeField]
+    private List<Sprite> _sprites;
+    private Dictionary<string, Sprite> sprites = new Dictionary<string, Sprite>();
 
     void Start(){
         behaviorState = BehaviorState.Idle;
         idleMoving = true; 
         movingDirection = Random.Range(1, 3);
-        StartCoroutine(Behave());
+        for (int i = 0; i< _sprites.Count; i++){
+            sprites.Add(_sprites[i].name, _sprites[i]);
+        }
     }
 
-    private void setRandomIdle(){
-        float chance = Random.Range(0, 100);
-        if (chance < 25){ // 75% chance it'll continue its current action, 25% chance it'll change
+    private void setRandomIdle(int chance){
+        float random = Random.Range(0, 100);
+        if (random < chance){ 
             idleMoving = !idleMoving;
         }
 
@@ -35,21 +45,33 @@ public class DogBehavior : MonoBehaviour
 
     private void Idle(){
         if (idleMoving){
-            if (transform.position.x >= 320){
+                image.sprite = sprites["dog_play"];
+            if (rt.anchoredPosition.x >= 320f){
                 movingDirection = Left;
+                setRandomIdle(50);
+                transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
             }
-            else if (transform.position.x <= -320){
+            else if (rt.anchoredPosition.x  <= -320f){
                 movingDirection = Right;
+                setRandomIdle(50);
+                transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+
+
             }
+            rt.anchoredPosition = new Vector2(rt.anchoredPosition.x + (2f * movingDirection), -150f);
+        }
+        else{
+            image.sprite = sprites["dog_idle"];
+            setRandomIdle(5);
+   
         }
     }
 
-    IEnumerator Behave(){
+    void FixedUpdate(){
+
         if (behaviorState == BehaviorState.Idle){
-            setRandomIdle();
             Idle();
         }
-        yield return null;
 
     }       
     
